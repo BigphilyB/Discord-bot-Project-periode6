11439ce999d2a73dbd372bcbdab3c084ebdc16bf
BigphilyB <philippe.bloemen@gmail.com>
2021-11-20 11:43:13 +0100

    ...

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client({ disableEveryone: false });
+const config = require("./data/config.json");
+
+bot.on("ready", async() => {
+    console.log(`${bot.user.username} started up successfully`)
+});
+
+bot.on("message", async message => {
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if(command == "ping") {
+        message.reply("Pong!")
+    }
+});
+
+bot.login(config.token)
